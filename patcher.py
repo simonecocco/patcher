0289fec799352e9499cd4d054ff9277c415253d6
simonecocco <38983939+simonecocco@users.noreply.github.com>
2022-06-26 21:26:03 +0200

    Add files via upload

--- patcher.py
+++ patcher.py
@@ -1,6 +1,7 @@
-#!/bin/bash python3
+#!/usr/bin/python3
 
 import sys
+from turtle import back
 from colorama import Fore, Back, Style
 import os
 from subprocess import call, Popen, PIPE
@@ -9,7 +10,7 @@ import re
 current_dir: str = os.getcwd() + '/'
 tab_char = '\t'
 new_line = '\n'
-VERSION: str = '1.0'
+VERSION: str = '1.2.0'
 
 # Stampa i crediti e la versione
 def print_credit() -> None:
@@ -27,7 +28,7 @@ def print_credit() -> None:
 def makefile_check(path: str) -> None:
     target: str = os.path.join(path, 'makefile')
     if not os.path.exists(target) or not os.path.isfile(target):
-        makefile = open(target, 'wb')
+        makefile = open(target, 'w')
         makefile.write('all: build up\n')
         makefile.write('build:\n\tsudo docker-compose build\n')
         makefile.write('up:\n\tsudo docker-compose up --build -d\n')
@@ -51,9 +52,10 @@ def print_help() -> None:
     OPZIONI
 
     -q -> non stampa i crediti
-    --no-bkp -> non fa il backup del file che si va a sostituire (solo con azione 'back')
+    --no-bkp -> non fa il backup del file che si va a sostituire
     --no-docker -> non fa il build del container
     --hard-build -> esegue un docker-compose down e poi up. Non funziona se --no-docker è presente
+    --back -> al posto di applicare la patch, torna una versione indietro per tutti i file (può essere usato solo con 'f')
 
     ''')
     sys.exit(0)
@@ -64,7 +66,8 @@ def validate(path: str, dir_allowed: bool=True) -> bool:
         if os.path.isfile(path) or dir_allowed:
             if not os.access(path, os.R_OK) or not os.access(path, os.W_OK):
                 call(['sudo', 'chmod', '-R', '777', path])
-                return os.path.isfile(path)
+                print(f"{path} is a {'file' if os.path.isfile(path) else 'dir'}")
+                return True
         else:
             print(Fore.RED + f'{path} is a directory' + Fore.RESET)
             sys.exit(1)
@@ -87,6 +90,15 @@ def get_differences(path_old: str, path_new: str) -> str:
             new.append(line)
     return f"{Fore.CYAN}{path_old}:{new_line}{(tab_char+new_line).join(old)}{new_line*2}{Fore.GREEN}{path_new}:{new_line}{(tab_char+new_line).join(new)}{Fore.RESET}"
 
+def dir_safe(dirpath: str, filepath: str) -> str:
+    validate(dirpath, dir_allowed=True)
+    if os.path.isdir(dirpath):
+        dirpath = os.path.join(dirpath, os.path.basename(filepath))
+        validate(dirpath, dir_allowed=False)
+        return dirpath
+    else:
+        return dirpath
+
 # esegue il backup del file
 def backup_file(path: str) -> None:
     path_n: int = 0
@@ -100,18 +112,18 @@ def backup_file(path: str) -> None:
     print(Fore.YELLOW + f'Per tornare indietro usa\nback {path} {path_n}' + Fore.RESET)
 
 # applica la patch
-def apply_patch(path_orig: str, path_new_file: str, docker_build: bool=True, quiet: bool=False, hard_build: bool=False) -> str:
+def apply_patch(path_orig: str, path_new_file: str, backup: bool=True, docker_build: bool=True, quiet: bool=False, hard_build: bool=False) -> str:
     print(f'patching {path_orig} with {path_new_file}')
-    validate(path_new_file, dir_allowed=False) # le directory non son consentite
-    if not validate(path_orig):
-        path_orig = path_orig + os.path.basename(path_new_file) # se è una directory usa il nome del file nuovo
+    validate(path_new_file, dir_allowed=False)
+    path_orig = dir_safe(path_orig, path_new_file)
     diff: str = get_differences(path_orig, path_new_file)
     risp: str = str(input(f"sei sicuro di voler applicare la patch? (y/n)\nGuarda le modifiche:\n{diff}\n")).strip()
     if not ('y' in risp) and not('Y' in risp):
         print('Patch non applicata')
         sys.exit(1)
+    print(path_orig)
     backup_file(path_orig)
-    call(['cp', path_new_file, path_orig])
+    call(['cp' if backup else 'mv', path_new_file, path_orig])
     makefile_path: str = path_orig.replace(current_dir, '') if current_dir in path_orig else path_orig
     makefile_path = makefile_path.split('/')[0]
     makefile_check(makefile_path)
@@ -143,7 +155,7 @@ def back2version(path: str, version: int, backup: bool=True, docker_build: bool=
         sys.exit(1)
     if backup:
         call(['mv', path, f'{path}.bkp{path_n}'])
-    call(['cp', target_path, path])
+    call(['cp' if backup else 'mv', target_path, path])
     makefile_path: str = path.replace(current_dir, '') if current_dir in path else path
     makefile_path = makefile_path.split('/')[0]
     makefile_check(makefile_path)
@@ -157,14 +169,17 @@ def back2version(path: str, version: int, backup: bool=True, docker_build: bool=
     return makefile_path
 
 # applica molteplici patch
-def parse_file(path: str, docker_build: bool=True, hard_build: bool=False) -> None:
+def parse_file(path: str, docker_build: bool=True, hard_build: bool=False, backup: bool=True, restore: bool=False) -> None:
     validate(path, dir_allowed=False)
+    print(f'leggo da {path}')
 
     patch = open(path, 'r')
     paths: list[str] = patch.readlines()
     last_makefile_path: str | None = None
+
     for patch_path in paths:
-        if len(patch_path) == 0 and docker_build and (last_makefile_path is not None):
+        patch_path = patch_path.strip()
+        if len(patch_path) <= 1 and docker_build and (last_makefile_path is not None):
             if hard_build:
                 print(Fore.YELLOW + f"hard reboot per il container {'/'.join(last_makefile_path.split('/')[:-1])}\n" + Fore.RESET)
                 call(['make', 'hard', '-C', last_makefile_path])
@@ -172,31 +187,46 @@ def parse_file(path: str, docker_build: bool=True, hard_build: bool=False) -> No
                 print(Fore.YELLOW + f"reboot per il container {'/'.join(last_makefile_path.split('/')[:-1])}\n" + Fore.RESET)
                 call(['make', '-C', last_makefile_path])
             last_makefile_path = None
-        else:
-            continue
-
-        path_orig, path_new_file = re.findall('^([\/\S\s]{1,})\s([\S\s]{1,})$', patch_path)[0]
-        print(Fore.YELLOW + '{path_orig} -> {path_new_file}' + Fore.RESET)
-        last_makefile_path = apply_patch(path_orig, path_new_file, docker_build=False, quiet=True)
+        elif len(patch_path) > 2:
+            try:
+                path_orig, path_new_file = re.findall('^([\/\S\s]{1,})\s([\S\s]{1,})$', patch_path)[0]
+                if not restore:
+                    print(Fore.YELLOW + f'{path_orig} -> {path_new_file}' + Fore.RESET)
+                    last_makefile_path = apply_patch(path_orig, path_new_file, docker_build=False, quiet=True, backup=backup)
+                else:
+                    print(Fore.YELLOW + f'{path_orig}' + Fore.RESET)
+                    last_makefile_path = back2version(dir_safe(path_orig, path_new_file), -1, backup=backup, docker_build=False)
+            except:
+                print(f'{path_orig} non applicata')
+    if docker_build and (last_makefile_path is not None):
+            if hard_build:
+                print(Fore.YELLOW + f"hard reboot per il container {'/'.join(last_makefile_path.split('/')[:-1])}\n" + Fore.RESET)
+                call(['make', 'hard', '-C', last_makefile_path])
+            else:
+                print(Fore.YELLOW + f"reboot per il container {'/'.join(last_makefile_path.split('/')[:-1])}\n" + Fore.RESET)
+                call(['make', '-C', last_makefile_path])
+            last_makefile_path = None
         
 # main
 if '-q' not in sys.argv:
     print_credit()
-if len(sys.argv) < 4 or 'help' in sys.argv:
+if len(sys.argv) < 3 or 'help' in sys.argv:
     print_help()
 
 recover_backup: bool = '--no-bkp' not in sys.argv
 docker_build: bool = '--no-docker' not in sys.argv
-hard_build: bool = '--hard-build' not in sys.argv
+hard_build: bool = '--hard-build' in sys.argv
+restore: bool = '--back' in sys.argv or '-b' in sys.argv
+
 action: str = sys.argv[1]
 first_arg: str = sys.argv[2]
-second_arg: str = sys.argv[3]
+second_arg: str | None = sys.argv[3] if action != 'f' and action != 'file' else None
 
 if action == 'apply' or action == 'a':
-    apply_patch(first_arg, second_arg, docker_build=docker_build, hard_build=hard_build)
+    apply_patch(first_arg, second_arg, docker_build=docker_build, hard_build=hard_build, backup=recover_backup)
 elif action == 'back' or action == 'b':
     back2version(first_arg, int(second_arg), backup=recover_backup, docker_build=docker_build, hard_build=hard_build)
 elif action == 'file' or action == 'f':
-    parse_file(first_arg, docker_build=docker_build, hard_build=hard_build) # todo
+    parse_file(first_arg, docker_build=docker_build, hard_build=hard_build, backup=recover_backup, restore=restore)
 else:
     print_help()
