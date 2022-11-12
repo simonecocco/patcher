4778ea4d4f2da0bd89ae34fa746f47f047d61530
simonecocco <coccosimone1234@gmail.com>
2022-11-12 15:20:22 +0100

    finiti i metodi per l'acquisizione dei servizi

--- service.py
+++ service.py
@@ -1,12 +1,15 @@
 from json import dumps, loads
 from os import path
-from utils import is_valid_file
+from utils import is_valid_file, call_process
+import log
+from sys import exit
+import re
 
 class Service:
     def __init__(self, directory: str, name: str, in_port: str, out_port: str, alias: str = '') -> None:
         self.path: str = directory
         self.name: str = name
-        self.alias: str = name if alias == '' else alias
+        self.alias: str = name if alias == '' or alias is None else alias
         self.port: tuple[int, int] = (int(in_port), int(out_port))
 
     def service_copy() -> int: #ritorna la versione
@@ -25,13 +28,35 @@ def __restorefromjson__(path: str) -> list[Service]:
         tmp = f.read()
         f.close()
     services_list: list[Service]
-    for service in loads(tmp):
-        s: Service = Service(**service)
+    return [Service(**service) for service in loads(tmp)]
 
 def __getfromdocker__(path: str) -> list[Service]:
-    pass
+    def get_ports(ports: str) -> list[str]:
+        ports = ports.split('/')[0]
+        ports = ports.split(':')[1]
+        return ports.split('->')
+
+    out, err = call_process('docker', 'ps', '--format', 'table ID:{{.ID}},NAME:{{.Image}},STATUS:{{.Status}},PORTS:{{.Ports}}')
+    if len(err) > 0:
+        log.error('Per configurare i processi è automaticamente è necessario essere utente root.')
+        exit(1)
+    out = out.split('\n')[1:]
+    regex: str = '^ID:([A-Fa-f0-9]{1,}),NAME:([\S]{1,}),STATUS:([\w\d()\s]{1,}),PORTS:([\d\.]{7}:\d{1,}->\d{1,}\/\w{1,})'
+    services_list: list[Service] = []
+    for serv_detail in out:
+        detail = re.findall(regex, serv_detail)
+        if len(detail) == 0:
+            continue
+        detail = detail[0]
+        services_list.append(Service(None, detail[1], *get_ports(detail[3])))
+    return services_list
+
+
+
+
 
 def __saveservices__(path: str, services: list[Service]) -> None:
+    tmp: list[dict] = [{'directory':s.path, 'name':s.name, 'in_port':str(s.port[0]), 'out_port':str(s.port[1]), 'alias':s.alias} for s in services]
     with open(path, 'w') as config:
         config.write(dumps(services))
         config.close()
