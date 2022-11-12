a57ff92c2d64d28086971193a3e3b5486c1609d9
simonecocco <coccosimone1234@gmail.com>
2022-11-12 12:08:40 +0100

    aggiunta restorefromjson

--- service.py
+++ service.py
@@ -3,10 +3,10 @@ from os import path
 from utils import is_valid_file
 
 class Service:
-    def __init__(self, directory: str, name: str, in_port: str, out_port: str) -> None:
+    def __init__(self, directory: str, name: str, in_port: str, out_port: str, alias: str = '') -> None:
         self.path: str = directory
         self.name: str = name
-        self.alias: str = name
+        self.alias: str = name if alias == '' else alias
         self.port: tuple[int, int] = (int(in_port), int(out_port))
 
     def service_copy() -> int: #ritorna la versione
@@ -20,19 +20,24 @@ class Service:
 JSON_FILE_NAME: str = 'services.json'
 
 def __restorefromjson__(path: str) -> list[Service]:
-    pass
+    tmp: str
+    with open(path, 'r') as f:
+        tmp = f.read()
+        f.close()
+    services_list: list[Service]
+    for service in loads(tmp):
+        s: Service = Service(**service)
 
 def __getfromdocker__(path: str) -> list[Service]:
     pass
 
 def __saveservices__(path: str, services: list[Service]) -> None:
-    save_path: str = path.join(path, JSON_FILE_NAME)
-    with open(save_path, 'w') as config:
+    with open(path, 'w') as config:
         config.write(dumps(services))
         config.close()
 
 def get_services(path: str) -> list[Service]:
     script_path: str = path.join(path, JSON_FILE_NAME)
-    tmp: list[Service] = __restorefromjson__(path) if is_valid_file(script_path) else __getfromdocker__(path)
-    __saveservices__(path, tmp)
+    tmp: list[Service] = __restorefromjson__(script_path) if is_valid_file(script_path) else __getfromdocker__(path)
+    __saveservices__(script_path, tmp)
     return tmp
