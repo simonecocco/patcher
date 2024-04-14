671d1e4beaa859dd847077ad8855e4fb4263ad8c
simonecocco <coccosimone@proton.me>
2024-04-14 13:31:12 +0200

    Piccolo refactoring

--- src/adpatcher/makefile.py
+++ src/adpatcher/makefile.py
@@ -1,4 +1,5 @@
 import os
+from os.path import exists
 import adpatcher.utils as utils
 from adpatcher.log import output, error, debug, warning
 from os.path import dirname
@@ -26,22 +27,26 @@ log:
 
 
 class Makefile:
+	__slots__: list = [
+		'verbose',
+		'service_mf_path'
+	]
+
 	def __init__(self, service_path: str, create_if_not_exist: bool=True, docker_v2=False, verbose=False) -> None:
 		"""
 		Crea un file makefile nel caso non ci sia
 		:param service_path path del servizio
 		:param create_if_not_exist 
 		"""
-		self.verbose = verbose
-		self.service_mf_path: str = service_path if service_path.endswith('makefile') else os.path.join(service_path, 'makefile')
-		if not utils.is_valid_file(self.service_mf_path):
-			mkfile_tmp = open(self.service_mf_path, 'w')
-			output(f'Creo makefile in {self.service_mf_path} (v2? {docker_v2})', verbose)
-			if not docker_v2:
-				mkfile_tmp.write(makefile_content)
-			else:
-				mkfile_tmp.write(makefile_content.replace('docker-compose', 'docker compose'))
-			mkfile_tmp.close()
+		self.verbose: bool = verbose
+		self.service_mf_path: str = utils.get_makefile_path_for_service(service_path)
+		if not exists(self.service_mf_path):
+			with open(self.service_mf_path, 'w') as mkfile_tmp:
+				output(f'Creo makefile in {self.service_mf_path} (v2? {docker_v2})', verbose)
+				if not docker_v2:
+					mkfile_tmp.write(makefile_content)
+				else:
+					mkfile_tmp.write(makefile_content.replace('docker-compose', 'docker compose'))
 
 	def __callmake__(self, target):
 		out, err = utils.call_process('make', ['-C', dirname(self.service_mf_path), target])
@@ -67,3 +72,6 @@ class Makefile:
 	def docker_softreboot(self):
 		out, err = self.__callmake__('soft')
 		return out, err
+
+	def __call__(self, action: str):
+		return self.__callmake__(action)
