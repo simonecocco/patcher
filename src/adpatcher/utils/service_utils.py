df658ba928e66d06ecf2c2097d4eafbe5a4a2811
Simone Cocco <coccosimone@proton.me>
2024-04-21 22:59:08 +0800

    Aggiustata la funzione di selezione del servizio dall'alias

--- src/adpatcher/utils/service_utils.py
+++ src/adpatcher/utils/service_utils.py
@@ -1,12 +1,13 @@
 from adpatcher.services import Service
 
-for target_service_alias in args.params[1:]:
-            current_selected_service: Service = select_service_based_on_alias(services, target_service_alias)
-            if current_selected_service is None:
-                error(f'Servizio non trovato: {target_service_alias}')
-                continue
-            warning(f'Servizio {current_selected_service.alias} in modalità sos')
-            current_selected_service.sos()
-            output(f'Servizio {current_selected_service.alias} in modalità sos completato', verbose=args.verbose)
+def select_service_by_alias(services_list: list[Service], alias: str) -> Service|None:
+    for service in services_list:
+        all_service_names: list[str] = [service.abs_disk_path, service.alias, service.name, str(service.port[1])]
+        if alias in all_service_names:
+            return service
+    else:
+        return None
 
-def select_service_by_alias(services_list: list, alias: str) -> Service:
+def get_service_alias_from_path(services_list: list, current_file_path: str) -> Service|None:
+    # TODO ottieni il servizio secondo quello che è presente nel path
+    pass
