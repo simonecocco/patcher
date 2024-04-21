857d39090fc2a7e3178ee3e1eb8415b1b801c55f
Simone Cocco <coccosimone@proton.me>
2024-04-22 01:24:21 +0800

    Aggiunte varie azioni

--- src/adpatcher/__main__.py
+++ src/adpatcher/__main__.py
@@ -1,18 +1,21 @@
-from os import getcwd
+from time import sleep
+from typing import Union
+from os import getcwd, stat
 from sys import argv
 from argparse import ArgumentParser
-from webbrowser import get
 from adpatcher.credits import print_credit
 from adpatcher.utils.docker_utils import *
 from adpatcher.utils.file_utils import is_valid_file
-from adpatcher.utils.stdout_utils import error, output
+from adpatcher.utils.stdout_utils import error, output, warning, debug
 from adpatcher.utils.service_utils import *
+from rich.live import Live
 
 def check_min_len(params: list, min_len: int) -> None:
     if len(params) < min_len:
         error(f'Numero minimo di parametri non rispettato. Attesi {min_len}, ricevuti {len(params)}')
         exit(1)
 
+# patcher configure
 def execute_configure_action(args) -> None:
     if len(args.params) == 1:
         create_docker_service_objects(path=getcwd(), verbose=args.verbose, dockerv2=args.docker2)
@@ -20,12 +23,30 @@ def execute_configure_action(args) -> None:
         print('Non sono supportate altre opzioni... per ora')
         exit(1)
 
+# patcher sos <list of services>
 def execute_sos_action(args, services: list[Service]) -> None:
-    pass
+    check_min_len(args.params, 2)
+    target_services: list[Service] = []
+    for service_name in args.params[1:]:
+        target_service: Union[Service, None] = select_service_by_alias(services, service_name)
+        if target_service is None:
+            if args.atomic:
+                error('Servizio non trovato')
+                exit(1)
+            else:
+                warning('Servizio non trovato')
+                continue
+        target_services.append(target_service)
+    for service in target_services:
+        warning(f'{service_name} in modalità emergenza')
+        service.sos()
 
 def execute_fix_action(args, services: list[Service]) -> None:
     pass
 
+# patcher services list
+# patcher services tarball
+# patcher services alias <service_name> <new_alias>
 def execute_services_action(args, services: list[Service]) -> None:
     check_min_len(args.params, 2)
     sub_action: str = args.params[1]
@@ -38,12 +59,53 @@ def execute_services_action(args, services: list[Service]) -> None:
         for service in services:
             output(f'Creo tarball per {service.name}', args.verbose)
             service.tarball(getcwd())
+    elif sub_action == 'alias':
+        check_min_len(args.params, 4)
+        service_name: str = args.params[2]
+        new_alias: str = args.params[3]
+        target_service: Union[Service, None] = select_service_by_alias(services, service_name)
+        if target_service is None:
+            error('Servizio non trovato')
+            exit(1)
+        target_service.alias = new_alias
+        output(f'Alias del servizio {target_service.name} cambiato in {new_alias}', args.verbose)
+        write_services_on_json(services, args.verbose)
 
 def execute_edit_action(args, services: list[Service]) -> None:
     pass
 
+# patcher sysadmin shadow <service name> <up time[seconds]> <down time[seconds]>
+# patcher sysadmin info
 def execute_sysadmin_action(args, services: list[Service]) -> None:
-    pass
+    check_min_len(args.params, 2)
+    sub_action: str = args.params[1]
+    if sub_action == 'shadow':
+        check_min_len(args.params, 3)
+        service_name: str = args.params[2]
+        target_service: Union[Service, None] = select_service_by_alias(services, service_name)
+        if target_service is None:
+            error('Servizio non trovato')
+            exit(1)
+        warning(f'Avvio modalità shadow')
+        while True:
+            sleep(int(args.params[3]))
+            warning('Stop del servizio')
+            target_service('down')
+            sleep(int(args.params[4]))
+            warning('Avvio del servizio')
+            target_service('shadow')
+    elif sub_action == 'info':
+        with Live(screen=True, auto_refresh=False) as live:
+            live.console.print('Informazioni sui servizi')
+            while True:
+                services_status: list = combine_services_with_infos(services, get_services_status(), get_services_usages())
+                status_string_to_print: str = 'Informazioni sui servizi\n\n'
+                for i, service_status in enumerate(services_status):
+                    status: int = service_status[1][1]
+                    partial_status_string_to_print: str = f"{i}) {service_status[0].name} ({service_status[0].alias})\n\t{service_status[1][0]}\n\tCPU%: {service_status[2]['cpu']}\n\tMEM%: {service_status[2]['ram']}"
+                    status_string_to_print += partial_status_string_to_print + '\n'
+                live.console.clear()
+                live.console.print(status_string_to_print)
 
 def execute_action(args, services: list[Service]) -> None:
     user_action: str = args.params[0]
@@ -99,4 +161,9 @@ def main() -> None:
         if is_valid_file(get_patcher_service_file_path())\
         else create_docker_service_objects(args.verbose, dockerv2=args.docker2)
 
-    execute_action(args, services)
+    try:
+        execute_action(args, services)
+    except:
+        print('Bye! :)')
+        exit(0)
+
