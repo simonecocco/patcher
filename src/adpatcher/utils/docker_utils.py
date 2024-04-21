1a57906af2d414de20adcecd16491581da2b0480
Simone Cocco <coccosimone@proton.me>
2024-04-22 01:24:48 +0800

    Aggiunte funzioni per sysadmin

--- src/adpatcher/utils/docker_utils.py
+++ src/adpatcher/utils/docker_utils.py
@@ -56,9 +56,7 @@ def scan_for_docker_services_in_filesystem(path: str='', verbose: bool=False) ->
 
     return docker_services_dir
 
-def create_docker_service_objects(path: str='', verbose: bool=False, dockerv2: bool=False) -> list:
-    docker_active_services: list = sorted(scan_for_docker_services(verbose), key=lambda service: service['name'])
-    docker_disk_services: list = sorted(scan_for_docker_services_in_filesystem(path=path, verbose=verbose), key=lambda service: service.split('/')[-1])
+def join_docker_services_with_their_disk_path(docker_active_services, docker_disk_services, dockerv2: bool=False, verbose=False) -> list[Service]:
     complete_service_list: list[Service] = []
     for docker_disk_service in docker_disk_services:
         output(f'Servizio trovato su disco: {docker_disk_service}', True)
@@ -81,7 +79,7 @@ def create_docker_service_objects(path: str='', verbose: bool=False, dockerv2: b
                     dockerv2=dockerv2, verbose=verbose
                 )
             )
-        else:
+        elif len(candidates) == 1:
             complete_service_list.append(
                 Service(
                     abspath(docker_disk_service),
@@ -91,10 +89,21 @@ def create_docker_service_objects(path: str='', verbose: bool=False, dockerv2: b
                     dockerv2=dockerv2, verbose=verbose
                 )
             )
+        else:
+            warning(f'Nessun candidato trovato per {docker_disk_service}')    
+    return complete_service_list
 
+def write_services_on_json(complete_service_list: list[Service], verbose: bool=False) -> None:
     with open(get_patcher_service_file_path(), 'w') as f:
         f.write(dumps([service.__dict__() for service in complete_service_list]))
     output(f'Servizi mappati e salvati in {get_patcher_service_file_path()}', verbose)
+
+def create_docker_service_objects(path: str='', verbose: bool=False, dockerv2: bool=False) -> list:
+    docker_active_services: list = sorted(scan_for_docker_services(verbose), key=lambda service: service['name'])
+    docker_disk_services: list = sorted(scan_for_docker_services_in_filesystem(path=path, verbose=verbose), key=lambda service: service.split('/')[-1])
+    complete_service_list: list[Service] = join_docker_services_with_their_disk_path(docker_active_services, docker_disk_services, dockerv2=dockerv2, verbose=verbose)
+    
+    write_services_on_json(complete_service_list, verbose=verbose)
     return complete_service_list
 
 def load_services_from_json(verbose: bool=False, dockerv2: bool=False) -> list:
@@ -107,3 +116,62 @@ def select_service_based_on_alias(services_list: list, alias: str) -> Service|No
         if alias in [service.alias, service.name, service.abs_disk_path]:
             return service
     return None
+
+def get_services_status():
+    raw_containers_status, _ = call_process('docker', ['ps', '-a', '--format', 'table {{.Names}}    {{.Status}}'])
+    containers_status = [
+        row.split('    ')
+        for row in raw_containers_status.split('\n')[1:]
+        if len(row) > 0
+    ]
+    return containers_status
+
+def get_services_usages():
+    raw_container_resources, _ = call_process('docker', ['stats', '--no-stream', '--format', 'table {{.Name}}    {{.CPUPerc}}    {{.MemPerc}}'])
+    container_resources = [
+        row.split('    ')
+        for row in raw_container_resources.split('\n')[1:]
+        if len(row) > 0
+    ]
+    return container_resources
+
+def valutate_container_status(status: str) -> list:
+    status = status.lower().split(' ')[0]
+    return {
+        'created': ['container up ma non avviato', 1],
+        'running': ['container completamente attivo', 1],
+        'paused': ['container up ma in pausa', 0],
+        'exited': ['container terminato', -1],
+        'dead': ['container morto', -1],
+        'restarting': ['container in riavvio', 0],
+        'removing': ['container in rimozione', -1],
+        'removed': ['container rimosso', -1],
+        'up': ['container completamente attivo', 1],
+        'down': ['container non in esecuzione', -1],
+        'oomkilled': ['container ucciso per troppa memoria consumata', -1],
+        'crashed': ['container crashato', -1]
+    }.get(status, [status, 0])
+
+def combine_services_with_infos(services: list[Service], containers_status: list, containers_resources: list) -> list:
+    services_status: list = []
+    #services = sorted(services, key=lambda service: service.name.lower())
+    #containers_status = sorted(containers_status, key=lambda container: container[0].lower())
+    #containers_resources = sorted(containers_resources, key=lambda container: container[0].lower())
+    for service in services:
+        complete_info: list = [service]
+        for container_status in containers_status:
+            if service.name.lower() in container_status[0].lower() or container_status[0].lower() in service.name.lower():
+                complete_info.append(valutate_container_status(container_status[1]))
+                break
+        else:
+            complete_info.append(['container non trovato', -1])
+        for container_resource in containers_resources:
+            if service.name.lower() in container_resource[0].lower() or container_resource[0].lower() in service.name.lower():
+                complete_info.append({'cpu':container_resource[1], 'ram':container_resource[2]})
+                break
+        else:
+            complete_info.append({'cpu':'non disponibile', 'ram':'non disponibile'})
+        if len(complete_info) == 3:
+            services_status.append(complete_info)
+    return services_status
+    
