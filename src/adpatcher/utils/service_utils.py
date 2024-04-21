b009ad95f60d8c953d9920febd86081f27c2a64c
Simone Cocco <coccosimone@proton.me>
2024-04-22 01:24:57 +0800

    Bugfix

--- src/adpatcher/utils/service_utils.py
+++ src/adpatcher/utils/service_utils.py
@@ -1,10 +1,13 @@
 from adpatcher.services import Service
+from typing import Union
 
-def select_service_by_alias(services_list: list[Service], alias: str) -> Service|None:
+def select_service_by_alias(services_list: list[Service], alias: str) -> Union[Service, None]:
+    alias = alias.lower()
     for service in services_list:
-        all_service_names: list[str] = [service.abs_disk_path, service.alias, service.name, str(service.port[1])]
-        if alias in all_service_names:
+        all_service_names: list[str] = [service.abs_disk_path.lower(), service.alias.lower(), service.name.lower(), str(service.port[1])]
+        if any(alias in name for name in all_service_names):
             return service
+    
     else:
         return None
 
