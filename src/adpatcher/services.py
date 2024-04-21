6560c923cd10d4e6dad628be8fdf6be313c344ef
Simone Cocco <coccosimone@proton.me>
2024-04-21 23:11:45 +0800

    Aggiunta funzione per la creazione di una tarball del servizio

--- src/adpatcher/services.py
+++ src/adpatcher/services.py
@@ -2,6 +2,7 @@ from adpatcher.makefile import Makefile
 from adpatcher.utils.file_utils import is_valid_file
 from adpatcher.utils.stdout_utils import warning
 from adpatcher.utils.git_utils import *
+from adpatcher.utils.process_utils import call_process
 from colorama import Fore, Back, Style
 from os import chdir, getcwd
 
@@ -104,3 +105,5 @@ class Service:
     def pretty_print(self):
         print(f'{Fore.GREEN if self.vulnerable_file == "UNKNOWN" else Fore.RED}{self.name} ({self.alias}) at {self.abs_disk_path} with port {self.port}{Style.RESET_ALL}')
 
+    def tarball(self, tarball_directory_output: str) -> None:
+        call_process('tar', ['-czvf', f'{tarball_directory_output}/{self.name}.tar.gz', self.abs_disk_path])
