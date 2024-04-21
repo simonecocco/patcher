0335430256257ec41ba6fdbbe92011e9eab1d1d6
Simone Cocco <coccosimone@proton.me>
2024-04-21 22:58:40 +0800

    Aggiunta funzione per gruppo docker

--- src/adpatcher/utils/permission_utils.py
+++ src/adpatcher/utils/permission_utils.py
@@ -1,4 +1,4 @@
-from os import geteuid
+from os import geteuid, getgroups, stat
 
 def is_root_user() -> bool:
     """
@@ -6,3 +6,11 @@ def is_root_user() -> bool:
     :return: True se è root
     """
     return geteuid() == 0
+
+def is_docker_user() -> bool:
+    """
+    Controlla se l'utente è aggiunto al gruppo docker
+    """
+    groups = getgroups()
+    docker_group_id = stat('/var/run/docker.sock').st_gid
+    return docker_group_id in groups
